95d7c799026d95622766a3b044b5574b8d2f2743
s1lent4gnt <kmeftah.khalil@gmail.com>
2025-01-19 23:30:02 +0100

    fix: correct deps version range syntax in setup.py

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
         "h5py",
         "termcolor",  # adept_envs dependency
         "click",  # adept_envs dependency
-        "1.0.3<=dm_control<=1.0.20",
+        "dm_control>=1.0.3,<=1.0.20",
         "mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl",
     ],
 )
